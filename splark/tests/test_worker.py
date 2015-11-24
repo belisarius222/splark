d92f05a701cc0509d3224f87312f91c5336c5995
Ted Blackman <ted.blackman@gmail.com>
2015-11-23 23:26:45 -0800

    testing cleanup

--- splark/tests/test_worker.py
+++ splark/tests/test_worker.py
@@ -9,114 +9,100 @@ from splark.worker.worker import Worker
 
 class WorkerWithSocket:
     def __init__(self):
-        self._w = Worker("tcp://localhost")
+        self.worker = Worker("tcp://localhost")
 
     def __enter__(self):
-        self._w.start()
-        self._skt = zmq.Context().socket(zmq.ROUTER)
-        self._skt.bind("tcp://*:23456")
-        assert self._skt.poll(1000, zmq.POLLIN), "No response from started worker"
+        self.worker.start()
+        self.socket = zmq.Context().socket(zmq.ROUTER)
+        self.socket.bind("tcp://*:23456")
+        assert self.socket.poll(1000, zmq.POLLIN), "No response from started worker"
 
-        self._wid = self._skt.recv_multipart()[0]
-        assert "worker-" in self._wid.decode("ascii")
-        assert self._w.is_alive()
+        self.worker_id = self.socket.recv_multipart()[0]
+        assert "worker-" in self.worker_id.decode("ascii")
+        assert self.worker.is_alive()
 
-        return self._w, self._wid, self._skt
+        return self
 
     def __exit__(self, arg1, arg2, arg3):
-        assert self._w.is_alive(), "Process Died During Testing"
-        self._skt.send_multipart((self._wid, b"", b"die"))
-        self._w.join()
+        assert self.worker.is_alive(), "Process Died During Testing"
+        self.socket.send_multipart((self.worker_id, b"", b"die"))
+        self.worker.join()
+
+    def transact_to_worker(self, *args, timeout=1000):
+        self.socket.send_multipart((self.worker_id, b"",) + args)
+        assert self.socket.poll(timeout, zmq.POLLIN), "Timeout waiting for response to: {}".format(args)
+        worker_id, _, response = self.socket.recv_multipart()
+        assert worker_id == self.worker_id
+        return response
 
 
 def test_worker_send_ping():
-    with WorkerWithSocket() as (wrkr, wid, skt):
-        skt.send_multipart((wid, b"", b"ping"))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
-        assert loads(pong) == "pong"
+    with WorkerWithSocket() as test_harness:
+        pong = test_harness.transact_to_worker(b"ping")
+        assert loads(pong) == "pong", loads(pong)
 
 
 def test_worker_set_data():
-    with WorkerWithSocket() as (wrkr, wid, skt):
+    with WorkerWithSocket() as test_harness:
         dataToSend = dumps(list(range(10)))
-
-        skt.send_multipart((wid, b"", b"setdata", b"daterz-idz", dataToSend))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
-        assert loads(pong)
+        pong = test_harness.transact_to_worker(b"setdata", b"daterz-idz", dataToSend)
+        assert loads(pong), pong
 
 
 def test_worker_set_get_data():
-    with WorkerWithSocket() as (wrkr, wid, skt):
+    with WorkerWithSocket() as test_harness:
         dataToSend = dumps(list(range(10)))
 
-        skt.send_multipart((wid, b"", b"setdata", b"daterz-idz", dataToSend))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
-        assert loads(pong)
+        pong = test_harness.transact_to_worker(b"setdata", b"daterz-idz", dataToSend)
+        assert loads(pong), pong
 
-        skt.send_multipart((wid, b"", b"getdata", b"daterz-idz"))
-        _, __, daterz2 = skt.recv_multipart()
-        print(loads(daterz2))
-        # NB: Comparing pickled strings of range(10)
-        assert dataToSend == daterz2
+        pickled_response = test_harness.transact_to_worker(b"getdata", b"daterz-idz")
+        assert dataToSend == pickled_response
 
 
 def test_worker_set_list_data():
-    with WorkerWithSocket() as (wrkr, wid, skt):
+    with WorkerWithSocket() as test_harness:
         dataToSend = dumps(list(range(10)))
 
-        dataID = b"daterz-idz"
-        skt.send_multipart((wid, b"", b"setdata", dataID, dataToSend))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
+        data_id = b"daterz-idz"
+        pong = test_harness.transact_to_worker(b"setdata", data_id, dataToSend)
         assert loads(pong)
 
-        skt.send_multipart((wid, b"", b"listdata"))
-        _, __, daterz2 = skt.recv_multipart()
-        listing = loads(daterz2)
+        pickled_listing = test_harness.transact_to_worker(b"listdata")
+        listing = loads(pickled_listing)
         assert len(listing) == 1
-        assert dataID in listing
+        assert data_id in listing
 
 
 def test_worker_set_call_get_data():
-    with WorkerWithSocket() as (wrkr, wid, skt):
-        data1 = list(range(10))
-        dataToSend = dumps(data1)
-
+    with WorkerWithSocket() as test_harness:
         print("Sending Data")
-        dataID = b"data1"
-        skt.send_multipart((wid, b"", b"setdata", dataID, dataToSend))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
+        data = list(range(10))
+        data_pickle = dumps(data)
+        data_id = b"data1"
+        pong = test_harness.transact_to_worker(b"setdata", data_id, data_pickle)
         assert loads(pong)
 
         print("Sending Function")
-        funID = b"fun1"
+        fun_id = b"fun1"
         fun = lambda x: x + 1
-        funPkl = toCP(fun)
-        skt.send_multipart((wid, b"", b"setdata", funID, funPkl))
-        assert skt.poll(1000, zmq.POLLIN)
-        id, _, pong = skt.recv_multipart()
+        fun_pickle = toCP(fun)
+        pong = test_harness.transact_to_worker(b"setdata", fun_id, fun_pickle)
         assert loads(pong)
 
         print("Sending Map Request")
-        skt.send_multipart((wid, b"", b"map", funID, dataID, b"data2"))
-        assert skt.poll(1000, zmq.POLLIN)
-        _, __, daterz2 = skt.recv_multipart()
+        map_output_id = b"data2"
+        pong = test_harness.transact_to_worker(b"map", fun_id, data_id, map_output_id)
+        assert loads(pong), pong
 
         print("\tReturned")
         time.sleep(1)
 
         print("Sending list")
-        skt.send_multipart((wid, b"", b"listdata"))
-        _, __, daterz2 = skt.recv_multipart()
-        listing = loads(daterz2)
+        listing_pickle = test_harness.transact_to_worker(b"listdata")
+        listing = loads(listing_pickle)
 
-        assert len(listing) == 3, listing
-        assert dataID in listing
+        assert set(listing) == {data_id, fun_id, map_output_id}, listing
 
-        skt.send_multipart((wid, b"", b"getdata", b"data2"))
-        _, __, daterz2 = skt.recv_multipart()
-        assert loads(daterz2) == list(map(fun, data1))
+        output_data_pickle = test_harness.transact_to_worker(b"getdata", map_output_id)
+        assert loads(output_data_pickle) == list(map(fun, data)), loads(output_data_pickle)
