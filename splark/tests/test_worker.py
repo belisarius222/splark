d3f9a454ad6ce82250865eaac5e5425eaf806d8f
Ted Blackman <ted.blackman@gmail.com>
2015-11-24 03:12:15 -0800

    Streamlined test calls

--- splark/tests/test_worker.py
+++ splark/tests/test_worker.py
@@ -19,91 +19,87 @@ class WorkerWithSocket:
         assert "worker-" in self.worker_id.decode("ascii")
         assert self.worker.is_alive()
 
-        return self
+        return self.send_and_recv
 
     def __exit__(self, arg1, arg2, arg3):
         assert self.worker.is_alive(), "Process Died During Testing"
         self.socket.send_multipart((self.worker_id, b"", b"die"))
         self.worker.join()
 
-    def transact_to_worker(self, *args, timeout=1000):
+    def send_and_recv(self, *args, timeout=1000):
         self.socket.send_multipart((self.worker_id, b"",) + args)
         assert self.socket.poll(timeout, zmq.POLLIN), "Timeout waiting for response to: {}".format(args)
         worker_id, _, response = self.socket.recv_multipart()
         assert worker_id == self.worker_id
-        return response
+        return fromCP(response)
 
 
 def test_worker_send_ping():
-    with WorkerWithSocket() as test_harness:
-        pong = test_harness.transact_to_worker(b"ping")
-        assert fromCP(pong) == "pong", fromCP(pong)
+    with WorkerWithSocket() as send_and_recv:
+        pong = send_and_recv(b"ping")
+        assert pong == "pong", pong
 
 
 def test_worker_set_data():
-    with WorkerWithSocket() as test_harness:
+    with WorkerWithSocket() as send_and_recv:
         dataToSend = toCP(list(range(10)))
-        pong = test_harness.transact_to_worker(b"setdata", b"daterz-idz", dataToSend)
-        assert fromCP(pong), pong
+        pong = send_and_recv(b"setdata", b"daterz-idz", dataToSend)
+        assert pong, "Invalid response from "
 
 
 def test_worker_set_get_data():
-    with WorkerWithSocket() as test_harness:
-        dataToSend = toCP(list(range(10)))
+    with WorkerWithSocket() as send_and_recv:
+        data = list(range(10))
+        data_pickle = toCP(data)
 
-        pong = test_harness.transact_to_worker(b"setdata", b"daterz-idz", dataToSend)
-        assert fromCP(pong), pong
+        response = send_and_recv(b"setdata", b"daterz-idz", data_pickle)
+        assert response is True, response
 
-        pickled_response = test_harness.transact_to_worker(b"getdata", b"daterz-idz")
-        assert dataToSend == pickled_response
+        response = send_and_recv(b"getdata", b"daterz-idz")
+        assert response == data, response
 
 
 def test_worker_set_list_data():
-    with WorkerWithSocket() as test_harness:
+    with WorkerWithSocket() as send_and_recv:
         dataToSend = toCP(list(range(10)))
 
         data_id = b"daterz-idz"
-        pong = test_harness.transact_to_worker(b"setdata", data_id, dataToSend)
-        assert fromCP(pong)
+        pong = send_and_recv(b"setdata", data_id, dataToSend)
+        assert pong is True, pong
 
-        pickled_listing = test_harness.transact_to_worker(b"listdata")
-        listing = fromCP(pickled_listing)
+        listing = send_and_recv(b"listdata")
         assert len(listing) == 1
         assert data_id in listing
 
 
 @timed(1)
 def test_worker_set_call_get_data():
-    with WorkerWithSocket() as test_harness:
+    with WorkerWithSocket() as send_and_recv:
         data = list(range(10))
         data_pickle = toCP(data)
         data_id = b"data1"
-        pong = test_harness.transact_to_worker(b"setdata", data_id, data_pickle)
-        assert fromCP(pong)
+        response = send_and_recv(b"setdata", data_id, data_pickle)
+        assert response is True, response
 
         fun_id = b"fun1"
         fun = lambda x: x + 1
         fun_pickle = toCP(fun)
-        pong = test_harness.transact_to_worker(b"setdata", fun_id, fun_pickle)
-        assert fromCP(pong)
+        response = send_and_recv(b"setdata", fun_id, fun_pickle)
+        assert response is True, response
 
         map_output_id = b"data2"
-        started_work_pickle = test_harness.transact_to_worker(b"map", fun_id, data_id, map_output_id)
-        started_work = fromCP(started_work_pickle)
+        started_work = send_and_recv(b"map", fun_id, data_id, map_output_id)
         assert type(started_work) == bool, "Malformed response from \"map\" command: {}".format(started_work)
         assert started_work is True, "Worker was unable to start \"map\" command."
 
         # Poll the worker repeatedly until it's done.
         isworking = True
         while isworking:
-            isworking_pickle = test_harness.transact_to_worker(b"isworking")
-            isworking = fromCP(isworking_pickle)
+            isworking = send_and_recv(b"isworking")
             assert type(isworking) == bool, "Malformed response from \"isworking\" command: {}".format(isworking)
 
-        listing_pickle = test_harness.transact_to_worker(b"listdata")
-        listing = fromCP(listing_pickle)
-
+        listing = send_and_recv(b"listdata")
         assert set(listing) == {data_id, fun_id, map_output_id}, listing
 
-        output_data_pickle = test_harness.transact_to_worker(b"getdata", map_output_id)
-        assert fromCP(output_data_pickle) == list(map(fun, data)), fromCP(output_data_pickle)
+        output_data = send_and_recv(b"getdata", map_output_id)
+        assert output_data == list(map(fun, data)), output_data
