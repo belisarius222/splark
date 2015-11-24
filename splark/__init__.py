e76c98347499b843228528d34cc80a5707a0be86
Ted Blackman <ted.blackman@gmail.com>
2015-11-23 22:54:30 -0800

    basic repo cleanup: removed old worker

--- splark/__init__.py
+++ splark/__init__.py
@@ -1,6 +1,6 @@
 from splark.Master import Master
 from splark.RDD import RDD
 from splark.SplarkContext import SplarkContext
-from splark.Worker import Worker
+from splark.worker.worker import Worker
 
 __all__ = [Master, RDD, SplarkContext, Worker]
